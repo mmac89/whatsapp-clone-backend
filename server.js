40a18a2d41850879269818f01597088a2efe68e4
Mathew <mmacelwee89@gmail.com>
2020-09-30 22:10:55 -0700

    working on routing for rooms on front end

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const pusher = new Pusher({
 
 //middleware
 app.use(express.json());
-//app.use(Cors);
 
     app.use((req,res,next)=> {
         res.setHeader('Access-Control-Allow-Origin', '*');
